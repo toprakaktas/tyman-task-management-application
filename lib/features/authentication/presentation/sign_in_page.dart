a0b07f3aca04eabd3181d80322bbb4df296fbf17
Toprak <topraktas19@hotmail.com>
2025-11-21 22:55:53 +0300

    add dark & light theme sign in and sign up pages

--- lib/features/authentication/presentation/sign_in_page.dart
+++ lib/features/authentication/presentation/sign_in_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/legacy.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tyman/core/constants/colors.dart';
 import 'package:tyman/core/providers/auth_providers.dart';
+import 'package:tyman/core/providers/theme_provider.dart';
 
 final signInPasswordObscuredProvider =
     StateProvider.autoDispose<bool>((ref) => true);
@@ -52,51 +53,73 @@ class SignInPageState extends ConsumerState<SignInPage> {
     final isEmailFocused = ref.watch(signInEmailFocusProvider);
     final isPasswordFocused = ref.watch(signInPasswordFocusProvider);
     final obscurePassword = ref.watch(signInPasswordObscuredProvider);
+    final themeMode = ref.watch(themeNotifierProvider);
+
+    final theme = Theme.of(context);
 
     return Scaffold(
       body: Container(
         decoration:
             BoxDecoration(gradient: BackgroundDecoration.getGradient(context)),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(45),
-              ),
-              color: Theme.of(context).cardColor,
-              shadowColor: Theme.of(context).shadowColor,
-              elevation: 15,
-              child: Padding(
-                padding: const EdgeInsets.all(25),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/images/userAvatar.png',
-                    ),
-                    const SizedBox(height: 50),
-                    textField(email, focusNode1, 'Email', Icons.email_rounded,
-                        isEmailFocused),
-                    const SizedBox(height: 10),
-                    textField(
-                      password,
-                      focusNode2,
-                      'Password',
-                      Icons.password_rounded,
-                      isPasswordFocused,
-                      isPasswordField: true,
-                      obscureText: obscurePassword,
-                      onToggleVisibility: () {
-                        final notifier =
-                            ref.read(signInPasswordObscuredProvider.notifier);
-                        notifier.state = !notifier.state;
-                      },
-                    ),
-                    const SizedBox(height: 8),
-                    account(),
-                    const SizedBox(height: 20),
-                    loginBottom()
-                  ],
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(45),
+                ),
+                color: theme.cardColor,
+                shadowColor: theme.shadowColor,
+                elevation: 15,
+                child: Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: IconButton(
+                          icon: Icon(
+                            themeMode == ThemeMode.dark
+                                ? Icons.light_mode
+                                : Icons.dark_mode,
+                          ),
+                          onPressed: () {
+                            ref
+                                .read(themeNotifierProvider.notifier)
+                                .toggleTheme();
+                          },
+                        ),
+                      ),
+                      Image.asset(
+                        'assets/images/userAvatar.png',
+                      ),
+                      const SizedBox(height: 50),
+                      textField(context, email, focusNode1, 'Email',
+                          Icons.email_rounded, isEmailFocused),
+                      const SizedBox(height: 10),
+                      textField(
+                        context,
+                        password,
+                        focusNode2,
+                        'Password',
+                        Icons.password_rounded,
+                        isPasswordFocused,
+                        isPasswordField: true,
+                        obscureText: obscurePassword,
+                        onToggleVisibility: () {
+                          final notifier =
+                              ref.read(signInPasswordObscuredProvider.notifier);
+                          notifier.state = !notifier.state;
+                        },
+                      ),
+                      const SizedBox(height: 8),
+                      account(context),
+                      const SizedBox(height: 20),
+                      loginBottom(context)
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -106,23 +129,25 @@ class SignInPageState extends ConsumerState<SignInPage> {
     );
   }
 
-  Widget account() {
+  Widget account(BuildContext context) {
+    final theme = Theme.of(context);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 19.5),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          const Text(
+          Text(
             "Don't you have an account?",
-            style: TextStyle(color: Colors.grey, fontSize: 14),
+            style: TextStyle(
+                color: theme.textTheme.bodyLarge!.color, fontSize: 14),
           ),
           const SizedBox(width: 5),
           GestureDetector(
             onTap: () => context.go('/signup'),
-            child: const Text(
+            child: Text(
               'Sign Up',
               style: TextStyle(
-                  color: Color(0xFF2D2D2D),
+                  color: theme.textTheme.bodyLarge!.color,
                   fontSize: 14,
                   fontWeight: FontWeight.bold),
             ),
@@ -132,24 +157,25 @@ class SignInPageState extends ConsumerState<SignInPage> {
     );
   }
 
-  Widget loginBottom() {
+  Widget loginBottom(BuildContext context) {
+    final theme = Theme.of(context);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 100),
       child: GestureDetector(
         onTap: () {
-          ref.watch(signInProvider)(email.text, password.text, context);
+          ref.read(signInProvider)(email.text, password.text, context);
         },
         child: Container(
           alignment: Alignment.center,
           width: double.infinity,
           height: 50,
           decoration: BoxDecoration(
-              color: const Color(0xFF3E3E3E),
+              color: theme.colorScheme.primary,
               borderRadius: BorderRadius.circular(10)),
-          child: const Text(
+          child: Text(
             'Sign In',
             style: TextStyle(
-              color: Color(0xFFAEAEAE),
+              color: theme.colorScheme.secondary,
               fontSize: 18.5,
               fontWeight: FontWeight.w500,
             ),
@@ -160,6 +186,7 @@ class SignInPageState extends ConsumerState<SignInPage> {
   }
 
   Widget textField(
+    BuildContext context,
     TextEditingController controller,
     FocusNode focusNode,
     String typeName,
@@ -169,22 +196,28 @@ class SignInPageState extends ConsumerState<SignInPage> {
     bool obscureText = false,
     VoidCallback? onToggleVisibility,
   }) {
+    final theme = Theme.of(context);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15),
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: theme.colorScheme.surface,
           borderRadius: BorderRadius.circular(15),
         ),
         child: TextField(
           controller: controller,
           focusNode: focusNode,
           obscureText: isPasswordField ? obscureText : false,
-          style: const TextStyle(
-              fontSize: 17, color: Color(0xFF3E3E3E), letterSpacing: 3),
+          style: TextStyle(
+              fontWeight: theme.textTheme.bodyMedium!.fontWeight,
+              fontSize: 17,
+              color: theme.textTheme.bodyMedium!.color,
+              letterSpacing: 3),
           decoration: InputDecoration(
             prefixIcon: Icon(icon,
-                color: isFocused ? const Color(0xFF414040) : Colors.blueGrey),
+                color: isFocused
+                    ? theme.colorScheme.primary
+                    : theme.unselectedWidgetColor),
             contentPadding:
                 const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
             hintText: typeName,
@@ -200,11 +233,11 @@ class SignInPageState extends ConsumerState<SignInPage> {
                 : null,
             enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(10),
-              borderSide: const BorderSide(color: Colors.blueGrey, width: 2.0),
+              borderSide: BorderSide(color: theme.cardColor, width: 2.0),
             ),
             focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(10),
-              borderSide: const BorderSide(color: Colors.black87, width: 2.0),
+              borderSide: BorderSide(color: theme.shadowColor, width: 2.0),
             ),
           ),
         ),
--- lib/features/authentication/presentation/sign_up_page.dart
+++ lib/features/authentication/presentation/sign_up_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/legacy.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tyman/core/constants/colors.dart';
 import 'package:tyman/core/providers/auth_providers.dart';
+import 'package:tyman/core/providers/theme_provider.dart';
 
 final signUpPasswordObscuredProvider =
     StateProvider.autoDispose<bool>((ref) => true);
@@ -68,87 +69,112 @@ class SignUpPageState extends ConsumerState<SignUpPage> {
     final obscurePassword = ref.watch(signUpPasswordObscuredProvider);
     final obscurePasswordConfirm =
         ref.watch(signUpPasswordConfirmObscuredProvider);
+    final themeMode = ref.watch(themeNotifierProvider);
+
+    final theme = Theme.of(context);
 
     return Scaffold(
         body: Container(
             decoration: BoxDecoration(
                 gradient: BackgroundDecoration.getGradient(context)),
-            child: Center(
-                child: SingleChildScrollView(
-                    child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(45),
-              ),
-              color: Theme.of(context).cardColor,
-              shadowColor: Theme.of(context).shadowColor,
-              elevation: 15,
-              child: Padding(
-                padding: const EdgeInsets.all(25),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(Icons.co_present_outlined,
-                        size: 60, color: Color(0xFF272525)),
-                    const SizedBox(height: 50),
-                    textField(email, focusNode1, 'Email', Icons.email_rounded,
-                        isEmailFocused),
-                    const SizedBox(height: 10),
-                    textField(
-                      password,
-                      focusNode2,
-                      'Password',
-                      Icons.password_rounded,
-                      isPasswordFocused,
-                      isPasswordField: true,
-                      obscureText: obscurePassword,
-                      onToggleVisibility: () {
-                        final notifier =
-                            ref.read(signUpPasswordObscuredProvider.notifier);
-                        notifier.state = !notifier.state;
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    textField(
-                      passwordConfirm,
-                      focusNode3,
-                      'Password Confirm',
-                      Icons.password_rounded,
-                      isPasswordConfirmFocused,
-                      isPasswordField: true,
-                      obscureText: obscurePasswordConfirm,
-                      onToggleVisibility: () {
-                        final notifier = ref.read(
-                            signUpPasswordConfirmObscuredProvider.notifier);
-                        notifier.state = !notifier.state;
-                      },
-                    ),
-                    const SizedBox(height: 8),
-                    account(),
-                    const SizedBox(height: 20),
-                    signUpBottom()
-                  ],
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Center(
+                  child: SingleChildScrollView(
+                      child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(45),
                 ),
-              ),
-            )))));
+                color: theme.cardColor,
+                shadowColor: theme.shadowColor,
+                elevation: 15,
+                child: Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: IconButton(
+                          icon: Icon(
+                            themeMode == ThemeMode.dark
+                                ? Icons.light_mode
+                                : Icons.dark_mode,
+                          ),
+                          onPressed: () {
+                            ref
+                                .read(themeNotifierProvider.notifier)
+                                .toggleTheme();
+                          },
+                        ),
+                      ),
+                      Icon(Icons.co_present_outlined,
+                          size: 60, color: theme.iconTheme.color),
+                      const SizedBox(height: 50),
+                      textField(context, email, focusNode1, 'Email',
+                          Icons.email_rounded, isEmailFocused),
+                      const SizedBox(height: 10),
+                      textField(
+                        context,
+                        password,
+                        focusNode2,
+                        'Password',
+                        Icons.password_rounded,
+                        isPasswordFocused,
+                        isPasswordField: true,
+                        obscureText: obscurePassword,
+                        onToggleVisibility: () {
+                          final notifier =
+                              ref.read(signUpPasswordObscuredProvider.notifier);
+                          notifier.state = !notifier.state;
+                        },
+                      ),
+                      const SizedBox(height: 10),
+                      textField(
+                        context,
+                        passwordConfirm,
+                        focusNode3,
+                        'Password Confirm',
+                        Icons.password_rounded,
+                        isPasswordConfirmFocused,
+                        isPasswordField: true,
+                        obscureText: obscurePasswordConfirm,
+                        onToggleVisibility: () {
+                          final notifier = ref.read(
+                              signUpPasswordConfirmObscuredProvider.notifier);
+                          notifier.state = !notifier.state;
+                        },
+                      ),
+                      const SizedBox(height: 8),
+                      account(context),
+                      const SizedBox(height: 20),
+                      signUpBottom(context)
+                    ],
+                  ),
+                ),
+              ))),
+            )));
   }
 
-  Widget account() {
+  Widget account(BuildContext context) {
+    final theme = Theme.of(context);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 19.5),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          const Text(
+          Text(
             "Do you have an account?",
-            style: TextStyle(color: Colors.grey, fontSize: 14),
+            style: TextStyle(
+                color: theme.textTheme.bodyLarge!.color, fontSize: 14),
           ),
           const SizedBox(width: 5),
           GestureDetector(
             onTap: () => context.go('/signin'),
-            child: const Text(
+            child: Text(
               'Sign In',
               style: TextStyle(
-                  color: Color(0xFF2D2D2D),
+                  color: theme.textTheme.bodyLarge!.color,
                   fontSize: 14,
                   fontWeight: FontWeight.bold),
             ),
@@ -158,12 +184,13 @@ class SignUpPageState extends ConsumerState<SignUpPage> {
     );
   }
 
-  Widget signUpBottom() {
+  Widget signUpBottom(BuildContext context) {
+    final theme = Theme.of(context);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 100),
       child: GestureDetector(
         onTap: () {
-          ref.watch(signUpProvider)(
+          ref.read(signUpProvider)(
               email.text, password.text, passwordConfirm.text, context);
         },
         child: Container(
@@ -171,12 +198,12 @@ class SignUpPageState extends ConsumerState<SignUpPage> {
           width: double.infinity,
           height: 50,
           decoration: BoxDecoration(
-              color: const Color(0xFF3E3E3E),
+              color: theme.colorScheme.primary,
               borderRadius: BorderRadius.circular(10)),
-          child: const Text(
+          child: Text(
             'Sign Up',
             style: TextStyle(
-                color: Color(0xFFAEAEAE),
+                color: theme.colorScheme.secondary,
                 fontSize: 18.5,
                 fontWeight: FontWeight.w500),
           ),
@@ -185,30 +212,34 @@ class SignUpPageState extends ConsumerState<SignUpPage> {
     );
   }
 
-  Widget textField(TextEditingController controller, FocusNode focusNode,
-      String typeName, IconData icon, bool isFocused,
+  Widget textField(BuildContext context, TextEditingController controller,
+      FocusNode focusNode, String typeName, IconData icon, bool isFocused,
       {bool isPasswordField = false,
       bool obscureText = false,
       VoidCallback? onToggleVisibility}) {
+    final theme = Theme.of(context);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15),
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: theme.colorScheme.surface,
           borderRadius: BorderRadius.circular(15),
         ),
         child: TextField(
           controller: controller,
           focusNode: focusNode,
           obscureText: isPasswordField ? obscureText : false,
-          style: const TextStyle(
+          style: TextStyle(
+              fontWeight: theme.textTheme.bodyMedium!.fontWeight,
               fontSize: 17,
-              color: Color(0xFF3E3E3E),
+              color: theme.textTheme.bodyMedium!.color,
               letterSpacing: 3,
               wordSpacing: 4),
           decoration: InputDecoration(
               prefixIcon: Icon(icon,
-                  color: isFocused ? const Color(0xFF414040) : Colors.blueGrey),
+                  color: isFocused
+                      ? theme.colorScheme.primary
+                      : theme.unselectedWidgetColor),
               contentPadding:
                   const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
               hintText: typeName,
@@ -222,13 +253,12 @@ class SignUpPageState extends ConsumerState<SignUpPage> {
                   : null,
               enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(10),
-                borderSide:
-                    const BorderSide(color: Colors.blueGrey, width: 2.0),
+                borderSide: BorderSide(color: theme.cardColor, width: 2.0),
               ),
               focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide:
-                      const BorderSide(color: Colors.black87, width: 2.0))),
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: theme.shadowColor, width: 2.0),
+              )),
         ),
       ),
     );
