f4403490ab21c64e65d18eb69c191af9577e9e1e
Toprak <topraktas19@hotmail.com>
2025-11-26 22:43:44 +0300

    refactor and enhance EditTaskDialog UI

--- lib/features/tasks/presentation/widgets/edit_task_dialog.dart
+++ lib/features/tasks/presentation/widgets/edit_task_dialog.dart
@@ -36,12 +36,23 @@ class _EditTaskDialogState extends State<EditTaskDialog> {
   }
 
   Future<void> _selectDate(BuildContext context) async {
+    final theme = Theme.of(context);
     final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: _dueDate,
-      firstDate: DateTime.now(),
-      lastDate: DateTime(2101),
-    );
+        context: context,
+        initialDate: _dueDate,
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2101),
+        builder: (context, child) {
+          return Theme(
+              data: theme.copyWith(
+                  cardTheme: theme.cardTheme,
+                  textTheme: theme.textTheme,
+                  colorScheme: theme.colorScheme,
+                  brightness: theme.brightness,
+                  iconButtonTheme: theme.iconButtonTheme,
+                  unselectedWidgetColor: theme.unselectedWidgetColor),
+              child: child!);
+        });
     if (picked != null) {
       setState(() => _dueDate = picked);
     }
@@ -49,66 +60,72 @@ class _EditTaskDialogState extends State<EditTaskDialog> {
 
   Future<void> _selectTime(BuildContext context) async {
     DateTime tempSelectedTime = DateTime.now();
-
+    final theme = Theme.of(context);
     await showCupertinoModalPopup(
         context: context,
-        builder: (_) => Container(
-              color: CupertinoColors.systemGrey6,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height / 3,
-                    child: CupertinoDatePicker(
-                      mode: CupertinoDatePickerMode.time,
-                      initialDateTime: DateTime.now(),
-                      onDateTimeChanged: (newTime) =>
-                          tempSelectedTime = newTime,
-                      use24hFormat: true,
-                      minuteInterval: 1,
+        builder: (_) => SafeArea(
+              bottom: true,
+              child: Container(
+                color: theme.colorScheme.secondary,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 3,
+                      child: CupertinoDatePicker(
+                        mode: CupertinoDatePickerMode.time,
+                        initialDateTime: DateTime.now(),
+                        onDateTimeChanged: (newTime) =>
+                            tempSelectedTime = newTime,
+                        use24hFormat: true,
+                        minuteInterval: 1,
+                      ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      CupertinoButton(
-                          onPressed: () => Navigator.of(context).pop(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CupertinoButton(
+                            onPressed: () => Navigator.of(context).pop(),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 10),
+                            child: const Text(
+                              'Cancel',
+                              style: TextStyle(color: deleteTaskColor),
+                            )),
+                        const SizedBox(width: 20),
+                        CupertinoButton(
+                          onPressed: () {
+                            setState(() => _dueTime =
+                                TimeOfDay.fromDateTime(tempSelectedTime));
+                            Navigator.pop(context);
+                          },
                           padding: const EdgeInsets.symmetric(
                               horizontal: 16, vertical: 10),
                           child: const Text(
-                            'Cancel',
-                            style: TextStyle(
-                                color: CupertinoColors.destructiveRed),
-                          )),
-                      const SizedBox(width: 16),
-                      CupertinoButton(
-                        onPressed: () {
-                          setState(() => _dueTime =
-                              TimeOfDay.fromDateTime(tempSelectedTime));
-                          Navigator.pop(context);
-                        },
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 16, vertical: 10),
-                        child: const Text(
-                          'OK',
-                          style: TextStyle(color: taskColor),
+                            'OK',
+                            style: TextStyle(color: taskColor),
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ));
   }
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return AlertDialog(
-      title: const Row(
+      title: Row(
         children: [
           Icon(CupertinoIcons.pen, color: taskColor),
           SizedBox(width: 10),
-          Text('Edit Task'),
+          Text(
+            'Edit Task',
+            style: TextStyle(color: theme.textTheme.bodyLarge!.color),
+          )
         ],
       ),
       content: SingleChildScrollView(
@@ -121,29 +138,32 @@ class _EditTaskDialogState extends State<EditTaskDialog> {
                   CupertinoIcons.collections,
                   color: taskColor,
                 ),
-                const SizedBox(
-                  width: 10,
-                ),
+                const SizedBox(width: 10),
                 Expanded(
                   child: DropdownButtonFormField<String>(
+                    borderRadius: BorderRadius.circular(25),
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    menuMaxHeight: kMinInteractiveDimension * 4.4,
+                    itemHeight: kMinInteractiveDimension,
+                    enableFeedback: true,
                     initialValue: _category,
                     icon: const Icon(Icons.arrow_drop_down_rounded),
                     elevation: 16,
-                    style: const TextStyle(
-                        color: CupertinoColors.darkBackgroundGray),
+                    style: TextStyle(color: theme.textTheme.bodyLarge!.color),
                     decoration: InputDecoration(
                       labelText: 'Category',
-                      labelStyle: const TextStyle(
-                          color: CupertinoColors.darkBackgroundGray),
+                      labelStyle:
+                          TextStyle(color: theme.textTheme.bodyLarge!.color),
                       enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: const BorderSide(color: Colors.grey)),
-                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.grey),
                         borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
                         borderSide: const BorderSide(color: taskColor),
+                        borderRadius: BorderRadius.circular(10),
                       ),
                       contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 10),
+                          horizontal: 13, vertical: 10),
                     ),
                     onChanged: (String? newValue) {
                       setState(() => _category = newValue!);
@@ -160,7 +180,7 @@ class _EditTaskDialogState extends State<EditTaskDialog> {
               ],
             ),
           ),
-          const SizedBox(height: 12),
+          const SizedBox(height: 10),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 8.0),
             child: Row(
@@ -169,11 +189,28 @@ class _EditTaskDialogState extends State<EditTaskDialog> {
                 const SizedBox(width: 10),
                 Expanded(
                     child: TextField(
+                        onTapOutside: (_) => FocusScope.of(context).unfocus(),
+                        cursorHeight: 20,
+                        minLines: 1,
+                        maxLines: 2,
+                        style: TextStyle(
+                            color: theme.textTheme.labelLarge!.color,
+                            fontWeight: theme.textTheme.labelLarge!.fontWeight,
+                            fontSize: theme.textTheme.labelLarge!.fontSize),
+                        cursorOpacityAnimates: true,
                         controller: _descriptionControlller,
-                        decoration: const InputDecoration(
+                        decoration: InputDecoration(
+                            isDense: true,
+                            floatingLabelStyle: TextStyle(
+                              color: theme.textTheme.bodyLarge!.color,
+                            ),
                             labelText: 'Description',
                             labelStyle: TextStyle(
-                                color: CupertinoColors.darkBackgroundGray),
+                              color: theme.textTheme.labelLarge!.color,
+                              fontWeight:
+                                  theme.textTheme.labelLarge!.fontWeight,
+                              fontSize: theme.textTheme.labelLarge!.fontSize,
+                            ),
                             enabledBorder: OutlineInputBorder(
                               borderRadius:
                                   BorderRadius.all(Radius.circular(10)),
@@ -187,13 +224,17 @@ class _EditTaskDialogState extends State<EditTaskDialog> {
               ],
             ),
           ),
-          const SizedBox(height: 12),
+          const SizedBox(height: 10),
           ListTile(
+            titleAlignment: ListTileTitleAlignment.center,
             leading:
                 const Icon(CupertinoIcons.calendar_today, color: taskColor),
             title: Text('Due Date: ${DateFormat('dd/MM/yy').format(_dueDate)}',
-                style:
-                    const TextStyle(color: CupertinoColors.darkBackgroundGray)),
+                style: TextStyle(
+                  color: theme.textTheme.labelLarge!.color,
+                  fontWeight: theme.textTheme.labelLarge!.fontWeight,
+                  fontSize: theme.textTheme.labelLarge!.fontSize,
+                )),
             contentPadding: EdgeInsets.zero,
             trailing: const Icon(Icons.keyboard_arrow_down),
             onTap: () => _selectDate(context),
@@ -203,7 +244,11 @@ class _EditTaskDialogState extends State<EditTaskDialog> {
             leading: const Icon(CupertinoIcons.time, color: taskColor),
             title: Text(
               'Due Time: ${_dueTime.hour}:${_dueTime.minute.toString().padLeft(2, '0')}',
-              style: const TextStyle(color: CupertinoColors.darkBackgroundGray),
+              style: TextStyle(
+                color: theme.textTheme.labelLarge!.color,
+                fontWeight: theme.textTheme.labelLarge!.fontWeight,
+                fontSize: theme.textTheme.labelLarge!.fontSize,
+              ),
             ),
             trailing: const Icon(Icons.keyboard_arrow_down),
             onTap: () => _selectTime(context),
@@ -212,9 +257,11 @@ class _EditTaskDialogState extends State<EditTaskDialog> {
       ),
       actions: [
         TextButton(
-          onPressed: () => Navigator.pop(context),
-          child: const Text('Cancel'),
-        ),
+            onPressed: () => Navigator.pop(context),
+            child: const Text(
+              'Cancel',
+              style: TextStyle(color: Colors.redAccent),
+            )),
         TextButton(
             onPressed: () {
               final updatedTask = TaskData(
@@ -231,8 +278,13 @@ class _EditTaskDialogState extends State<EditTaskDialog> {
                   completed: widget.task.completed);
               Navigator.of(context).pop(updatedTask);
             },
-            child: const Text('Save'))
+            child: const Text('Save',
+                style:
+                    TextStyle(color: taskColor, fontWeight: FontWeight.bold)))
       ],
+      backgroundColor: theme.colorScheme.secondary,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20))),
     );
   }
 }
