e0f823c9484be3a31a72bb5b3a136200639077f9
Toprak <topraktas19@hotmail.com>
2025-11-28 23:57:01 +0300

    refactor update DetailSliverAppBar colors to be dynamic

--- lib/features/tasks/presentation/widgets/detail_sliver_app_bar.dart
+++ lib/features/tasks/presentation/widgets/detail_sliver_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tyman/data/models/task_model.dart';
 
 class DetailSliverAppBar extends StatelessWidget {
   final String category;
@@ -12,32 +13,47 @@ class DetailSliverAppBar extends StatelessWidget {
     final todayTaskText = taskLeft == 1
         ? '$taskLeft task for today!'
         : '$taskLeft tasks for today!';
+    final TaskModel taskModel = TaskModel.fromTitle(category);
+    final theme = Theme.of(context);
 
     return SliverAppBar(
       pinned: true,
       expandedHeight: 90,
-      backgroundColor: Colors.black,
+      backgroundColor: taskModel.btnColor,
       leading: IconButton(
         onPressed: () => Navigator.of(context).maybePop(true),
         icon: const Icon(Icons.arrow_back_ios_new_rounded),
         iconSize: 20,
+        color: taskModel.iconColor,
+        enableFeedback: true,
       ),
       flexibleSpace: FlexibleSpaceBar(
         title: Column(
+          spacing: 5,
           mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               '$category tasks',
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              style: TextStyle(
+                  fontWeight: theme.textTheme.bodyLarge?.fontWeight,
+                  fontSize: theme.textTheme.bodyLarge?.fontSize,
+                  color: taskModel.iconColor.withValues(
+                    red: taskModel.iconColor.r + 66,
+                    green: taskModel.iconColor.g + 66,
+                    blue: taskModel.iconColor.b + 66,
+                  )),
             ),
-            const SizedBox(height: 5),
             Text(
               todayTaskText,
               style: TextStyle(
                   fontSize: 12,
-                  color: Colors.grey[600],
-                  fontWeight: FontWeight.normal),
+                  color: taskModel.iconColor.withValues(
+                    red: taskModel.iconColor.r + 66,
+                    green: taskModel.iconColor.g + 66,
+                    blue: taskModel.iconColor.b + 66,
+                  ),
+                  fontWeight: theme.textTheme.bodySmall?.fontWeight),
             )
           ],
         ),
