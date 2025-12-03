4f216dca7d6a6796a612126c2379316f22c13451
Toprak <topraktas19@hotmail.com>
2025-12-04 00:41:03 +0300

    refactor: improve delete task dialog UI

--- lib/features/tasks/presentation/widgets/delete_task_dialog.dart
+++ lib/features/tasks/presentation/widgets/delete_task_dialog.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tyman/core/constants/colors.dart';
 
 Future<bool?> deleteTaskDialog(BuildContext context) {
   return showDialog<bool>(
       context: context,
       builder: (context) => AlertDialog(
+              insetPadding: EdgeInsets.all(16),
               title: const Row(
                 children: [
                   Icon(CupertinoIcons.trash),
                   SizedBox(width: 10),
-                  Text('Delete Task',
-                      style: TextStyle(color: CupertinoColors.destructiveRed)),
+                  Text('Delete Task', style: TextStyle(color: deleteTaskColor)),
                 ],
               ),
-              content: const Text('Are you sure you want to delete this task?'),
+              content: const Text(
+                'Are you sure you want to delete this task?',
+                textAlign: TextAlign.center,
+              ),
+              contentPadding: EdgeInsets.all(16),
               actions: [
                 TextButton(
                   onPressed: () => Navigator.of(context).pop(false),
                   child: const Text('Cancel',
-                      style: TextStyle(color: Colors.blue)),
+                      style: TextStyle(color: Colors.blue, fontSize: 16)),
                 ),
                 TextButton(
                     onPressed: () => Navigator.of(context).pop(true),
                     child: const Text('Delete',
-                        style: TextStyle(
-                            color: CupertinoColors.destructiveRed,
-                            fontSize: 13)))
+                        style: TextStyle(color: deleteTaskColor, fontSize: 16)))
               ]));
 }
