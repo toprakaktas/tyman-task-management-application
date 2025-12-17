bdd9b13e2048ff2eb357e029efbd992727fd4731
Toprak <topraktas19@hotmail.com>
2025-12-17 18:13:26 +0300

    refactor: improve snackbar UI

--- lib/core/utils/snackbar_helper.dart
+++ lib/core/utils/snackbar_helper.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String message) {
+  final theme = Theme.of(context);
   if (context.mounted) {
-    ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text(message)));
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      backgroundColor: theme.shadowColor,
+      elevation: 4,
+      behavior: SnackBarBehavior.floating,
+      margin: const EdgeInsets.all(16),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      content: Row(
+        spacing: 10,
+        children: [
+          Icon(Icons.info_rounded,
+              color: theme.colorScheme.secondary, size: 24),
+          Text(
+            message,
+            maxLines: 2,
+            style: theme.textTheme.bodyMedium?.copyWith(
+                fontWeight: FontWeight.w500,
+                fontSize: 16,
+                color: theme.colorScheme.secondary),
+          )
+        ],
+      ),
+      duration: const Duration(seconds: 3),
+    ));
   }
 }
