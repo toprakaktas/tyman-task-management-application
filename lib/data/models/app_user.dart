b1e699b15944a7f66684ada53cc1b4b9a3198b67
Toprak <topraktas19@hotmail.com>
2025-12-27 22:12:58 +0300

    feat: add notification settings to user model

--- lib/data/models/app_user.dart
+++ lib/data/models/app_user.dart
@@ -3,20 +3,22 @@ class AppUser {
   String name;
   final String email;
   String photo;
+  bool isNotificationsEnabled;
 
   AppUser({
     required this.uid,
     required this.name,
     required this.email,
     this.photo = 'assets/images/userAvatar.png',
+    this.isNotificationsEnabled = true,
   });
 
   factory AppUser.fromFirestore(Map<String, dynamic> data, String uid) {
     return AppUser(
-      uid: uid,
-      name: data['name'] ?? 'Earthling',
-      email: data['email'] ?? '',
-      photo: data['photoUrl'] as String? ?? 'assets/images/userAvatar.png',
-    );
+        uid: uid,
+        name: data['name'] ?? 'Earthling',
+        email: data['email'] ?? '',
+        photo: data['photoUrl'] as String? ?? 'assets/images/userAvatar.png',
+        isNotificationsEnabled: data['notificationsEnabled'] ?? true);
   }
 }
